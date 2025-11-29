5493dc50baa1d2b06f2afd946c4ffca1f9e36579
tanjirokamado090 <tarunadhikary20@gmail.com>
2025-11-29 12:57:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -42,9 +42,9 @@ class EnterpriseConfigurationManager {
              */
             bot: {
                 prefix: EnvironmentVariableProcessor.BOT_PREFIX || "!",  // 👈 prefix (!, ?, etc)
-                ownerIds: ["1443553022520197133"],      // 👈 ADD YOUR DISCORD ID HERE
+                ownerIds: ["1425397352235139138"],      // 👈 ADD YOUR DISCORD ID HERE
                 embedColor: 0x00AE86,               // 👈 Bot embed color (hex)
-                supportServer: "https://discord.gg/UumG6vXsU",    // 👈 Your support server link
+                supportServer: "https://discord.gg/FeFvxTngs",    // 👈 Your support server link
                 defaultStatus: "🎵 Ready for music!"         // 👈 Bot status message
             },
             
@@ -101,6 +101,7 @@ module.exports = primaryApplicationConfiguration;
  * Never share your bot token or database URI publicly!
  * Use environment variables in production!
  */
+
 
 
 
